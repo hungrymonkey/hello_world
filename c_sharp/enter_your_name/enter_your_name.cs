3f9f18bda0fa4013c267ca7f35205e05eedf08bb
hungrymonkey <tedchang2010@gmail.com>
2019-04-21 22:11:41 -0700

    added links explaining the difficulty of validating names added xkcd link
    used this url https://docs.microsoft.com/en-us/dotnet/api/system.text.regularexpressions.match?view=netframework-4.8

--- c_sharp/enter_your_name/enter_your_name.cs
+++ c_sharp/enter_your_name/enter_your_name.cs
@@ -1,16 +1,32 @@
 // A simple reply your name
 using System;
+using System.Text.RegularExpressions;
 namespace HelloWorld
 {
     class Hello 
-    {
+    {	
+
+	static bool validateName(String s)
+	{
+	    /*
+            * https://xkcd.com/327/ 
+            */
+            MatchCollection matches = Regex.Matches(s, "[;+=<>\\/]");
+	    foreach (Match match in matches) return false; return true;
+	}
         static void Main() 
         {
 
             // Keep the console window open in debug mode.
             Console.Write("Please Enter Your Name and Press Enter: ");
             String name = Console.ReadLine();
-            Console.WriteLine("Your Name is " + name);
+           /*
+            * Validating names is not simple https://www.kalzumeus.com/2010/06/17/falsehoods-programmers-believe-about-names/
+            */
+            if( validateName(name ))
+               Console.WriteLine("Your Name is " + name);
+            else
+               Console.WriteLine("Please provide legal evidence for your name " + name);
         }
     }
 }
